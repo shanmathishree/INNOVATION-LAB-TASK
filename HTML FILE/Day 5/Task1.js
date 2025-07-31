bbbbf4c20d69ccbefc162f8659d7416a1e553aa2
shanmathishree <shreeshanmathi108@gmail.com>
2025-07-31 21:26:24 +0530

    Task1.js

---
+++ HTML FILE/Day 5/Task1.js
@@ -0,0 +1,13 @@
+Task 1: Advanced Array Methods (45 mins)
+
+Write examples that:
+
+Use filter() to get a subset of array elements (e.g., numbers > 10)
+
+Use reduce() to calculate the sum, product, or concatenate strings
+
+Use slice() to extract a portion of an array without modifying the original
+
+Use splice() to remove and/or add elements in an array
+
+Use the spread operator to clone an array and merge two arrays

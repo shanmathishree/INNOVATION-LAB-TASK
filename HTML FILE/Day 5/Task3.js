65f80ebb5bffd5fd0e26faef4cb57643b2d36e94
shanmathishree <shreeshanmathi108@gmail.com>
2025-07-31 21:28:22 +0530

    Task3.js

---
+++ HTML FILE/Day 5/Task3.js
@@ -0,0 +1,9 @@
+Task 3: Object Destructuring and Spread (15 mins)
+
+Use destructuring to extract properties from objects into variables
+
+Use destructuring with default values and nested objects
+
+Use the spread operator to clone and merge objects
+
+Write short code snippets demonstrating these concepts

56e89ba39abee7b81f8864dca3ef034f7d89ea62
shanmathishree <shreeshanmathi108@gmail.com>
2025-07-31 19:43:30 +0530

    Task2.js

--- HTML FILE/DAY 3/Task2.js
+++ HTML FILE/DAY 3/Task2.js
@@ -1,23 +1,14 @@
-//1. Convert traditional function declarations into function expressions:
-
-function add(a, b) {
-    return a + b;
+//---Function expression & Arrow functions---//
+//---Declaration---//
+function add(x,y){
+    return x+y;
 }
-console.log(add(5, 3));  
-const add = function (a, b) 
-{
-    return a + b;
+
+//---Expression---//
+const add=function(x,y){
+    return x+y;
 };
-console.log(add(5, 3)); 
+console.log(add(2,3));
 
-
-
-//2. Rewrite functions using ES6 arrow syntax:
-
-const multiply = (a, b) => {
-  return a * b;
-};
-console.log(multiply(4, 2)); 
-
-const square = n => n * n;
-console.log(square(5)); 
+const add=(x,y)=>x+y;
+console.log(add(2,3));

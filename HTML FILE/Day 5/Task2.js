324d7626b1f0eddb253e161ec29c0d4f6e7f161e
shanmathishree <shreeshanmathi108@gmail.com>
2025-07-31 21:27:30 +0530

    Task2.js

---
+++ HTML FILE/Day 5/Task2.js
@@ -0,0 +1,7 @@
+Task 2: Working with Objects and Methods (30 mins)
+
+Create objects representing real-world entities (e.g., a student with name, age, and a method to display info)
+
+Add functions inside objects as methods and invoke them
+
+Explore accessing and modifying obiect properties
